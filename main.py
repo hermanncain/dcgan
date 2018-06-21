4e1efcd77ff874686233121d245d1d52f2e1b1f7
hermanncain <397392721@qq.com>
2018-06-21 19:11:07 +0800

    cgan

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from random import shuffle
 from model import *
 from utils import *
 
+import win_unicode_console
+win_unicode_console.enable()
+
 pp = pprint.PrettyPrinter()
 
 """
-TensorLayer implementation of DCGAN to generate face image.
+TensorLayer implementation of DCGAN to generate mnist image.
 
 Usage : see README.md
 """
@@ -19,18 +22,18 @@ flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
-flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+#flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The number of batch images [64]")
-flags.DEFINE_integer("image_size", 108, "The size of image to use (will be center cropped) [108]")
-flags.DEFINE_integer("output_size", 64, "The size of the output images to produce [64]")
+#flags.DEFINE_integer("image_size", 28, "The size of image to use (will be center cropped) [108]")
+flags.DEFINE_integer("output_size", 28, "The size of the output images to produce [64]")
 flags.DEFINE_integer("sample_size", 64, "The number of sample images [64]")
-flags.DEFINE_integer("c_dim", 3, "Dimension of image color. [3]")
+flags.DEFINE_integer("c_dim", 1, "Dimension of image color. [1]")
 flags.DEFINE_integer("sample_step", 500, "The interval of generating sample. [500]")
 flags.DEFINE_integer("save_step", 500, "The interval of saveing checkpoints. [500]")
-flags.DEFINE_string("dataset", "celebA", "The name of dataset [celebA, mnist, lsun]")
+flags.DEFINE_string("dataset", "mnist", "The name of dataset [celebA, mnist, lsun]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
-flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
+flags.DEFINE_boolean("is_train", True, "True for training, False for testing [False]")
 flags.DEFINE_boolean("is_crop", True, "True for training, False for testing [False]")
 flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 FLAGS = flags.FLAGS
@@ -44,23 +47,25 @@ def main(_):
     z_dim = 100
     with tf.device("/gpu:0"):
         ##========================= DEFINE MODEL ===========================##
+        data_X,data_y = load_mnist()
         z = tf.placeholder(tf.float32, [FLAGS.batch_size, z_dim], name='z_noise')
+        y = tf.placeholder(tf.float32, [FLAGS.batch_size, 10], name='y')
         real_images =  tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.output_size, FLAGS.output_size, FLAGS.c_dim], name='real_images')
 
         # z --> generator for training
-        net_g, g_logits = generator_simplified_api(z, is_train=True, reuse=False)
-        # generated fake images --> discriminator
-        net_d, d_logits = discriminator_simplified_api(net_g.outputs, is_train=True, reuse=False)
+        net_g, g_logits = generator_simplified_api(z, y, is_train=True, reuse=False)
         # real images --> discriminator
-        net_d2, d2_logits = discriminator_simplified_api(real_images, is_train=True, reuse=True)
+        net_dr, dr_logits = discriminator_simplified_api(real_images, y, is_train=True, reuse=False)
         # sample_z --> generator for evaluation, set is_train to False
         # so that BatchNormLayer behave differently
-        net_g2, g2_logits = generator_simplified_api(z, is_train=False, reuse=True)
+        net_gs, gs_logits = generator_simplified_api(z, y, is_train=False, reuse=True)
+        # generated fake images --> discriminator
+        net_d, d_logits = discriminator_simplified_api(net_g.outputs, y, is_train=True, reuse=True)
 
         ##========================= DEFINE TRAIN OPS =======================##
         # cost for updating discriminator and generator
         # discriminator: real images are labelled as 1
-        d_loss_real = tl.cost.sigmoid_cross_entropy(d2_logits, tf.ones_like(d2_logits), name='dreal')
+        d_loss_real = tl.cost.sigmoid_cross_entropy(dr_logits, tf.ones_like(dr_logits), name='dreal')
         # discriminator: images from generator (fake) are labelled as 0
         d_loss_fake = tl.cost.sigmoid_cross_entropy(d_logits, tf.zeros_like(d_logits), name='dfake')
         d_loss = d_loss_real + d_loss_fake
@@ -91,45 +96,40 @@ def main(_):
     net_g_name = os.path.join(save_dir, 'net_g.npz')
     net_d_name = os.path.join(save_dir, 'net_d.npz')
 
-    data_files = glob(os.path.join("./data", FLAGS.dataset, "*.jpg"))
-
-    sample_seed = np.random.normal(loc=0.0, scale=1.0, size=(FLAGS.sample_size, z_dim)).astype(np.float32)# sample_seed = np.random.uniform(low=-1, high=1, size=(FLAGS.sample_size, z_dim)).astype(np.float32)
+    sample_seed = np.random.uniform(-1, 1, size=(FLAGS.sample_size , z_dim))
 
     ##========================= TRAIN MODELS ================================##
     iter_counter = 0
     for epoch in range(FLAGS.epoch):
-        ## shuffle data
-        shuffle(data_files)
 
-        ## update sample files based on shuffled data
-        sample_files = data_files[0:FLAGS.sample_size]
-        sample = [get_image(sample_file, FLAGS.image_size, is_crop=FLAGS.is_crop, resize_w=FLAGS.output_size, is_grayscale = 0) for sample_file in sample_files]
-        sample_images = np.array(sample).astype(np.float32)
+        sample_images = data_X[0:FLAGS.sample_size]
+        sample_labels = data_y[0:FLAGS.sample_size]
+
         print("[*] Sample images updated!")
 
         ## load image data
-        batch_idxs = min(len(data_files), FLAGS.train_size) // FLAGS.batch_size
+        batch_idxs = len(data_X) // FLAGS.batch_size
 
-        for idx in range(0, batch_idxs):
-            batch_files = data_files[idx*FLAGS.batch_size:(idx+1)*FLAGS.batch_size]
-            ## get real images
-            # more image augmentation functions in http://tensorlayer.readthedocs.io/en/latest/modules/prepro.html
-            batch = [get_image(batch_file, FLAGS.image_size, is_crop=FLAGS.is_crop, resize_w=FLAGS.output_size, is_grayscale = 0) for batch_file in batch_files]
-            batch_images = np.array(batch).astype(np.float32)
-            batch_z = np.random.normal(loc=0.0, scale=1.0, size=(FLAGS.sample_size, z_dim)).astype(np.float32)  # batch_z = np.random.uniform(low=-1, high=1, size=(FLAGS.batch_size, z_dim)).astype(np.float32)
+        for idx in range(0, int(batch_idxs)):
+            
+            batch_images = data_X[idx*FLAGS.batch_size:(idx+1)*FLAGS.batch_size]
+            batch_labels = data_y[idx*FLAGS.batch_size:(idx+1)*FLAGS.batch_size]
+            batch_z = np.random.uniform(-1, 1, [FLAGS.batch_size, z_dim]) \
+              .astype(np.float32)
             start_time = time.time()
+
             # updates the discriminator
-            errD, _ = sess.run([d_loss, d_optim], feed_dict={z: batch_z, real_images: batch_images })
+            errD, _ = sess.run([d_loss, d_optim], feed_dict={z: batch_z, y:batch_labels, real_images: batch_images })
             # updates the generator, run generator twice to make sure that d_loss does not go to zero (difference from paper)
             for _ in range(2):
-                errG, _ = sess.run([g_loss, g_optim], feed_dict={z: batch_z})
+                errG, _ = sess.run([g_loss, g_optim], feed_dict={z: batch_z, y:batch_labels})
             print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
                     % (epoch, FLAGS.epoch, idx, batch_idxs, time.time() - start_time, errD, errG))
 
             iter_counter += 1
             if np.mod(iter_counter, FLAGS.sample_step) == 0:
                 # generate and visualize generated images
-                img, errD, errG = sess.run([net_g2.outputs, d_loss, g_loss], feed_dict={z : sample_seed, real_images: sample_images})
+                img, errD, errG = sess.run([net_gs.outputs, d_loss, g_loss], feed_dict={z : sample_seed, real_images: sample_images, y:sample_labels})
                 tl.visualize.save_images(img, [8, 8], './{}/train_{:02d}_{:04d}.png'.format(FLAGS.sample_dir, epoch, idx))
                 print("[Sample] d_loss: %.8f, g_loss: %.8f" % (errD, errG))
 
--- model.py
+++ model.py
@@ -2,80 +2,158 @@
 import tensorflow as tf
 import tensorlayer as tl
 from tensorlayer.layers import *
+import os
+import numpy as np
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
-def generator_simplified_api(inputs, is_train=True, reuse=False):
-    image_size = 64
-    s2, s4, s8, s16 = int(image_size/2), int(image_size/4), int(image_size/8), int(image_size/16)
+def generator_simplified_api(inputs, y=None, is_train=True, reuse=False):
+    image_size = 28
     gf_dim = 64 # Dimension of gen filters in first conv layer. [64]
-    c_dim = FLAGS.c_dim # n_color 3
+    y_dim = 10
+    c_dim = FLAGS.c_dim # n_color 1
     batch_size = FLAGS.batch_size # 64
     w_init = tf.random_normal_initializer(stddev=0.02)
+    w_initl = tf.random_normal_initializer(stddev=0.02)
+    b_init = tf.constant_initializer(0.0)
     gamma_init = tf.random_normal_initializer(1., 0.02)
+
+    s_h, s_w = image_size, image_size
+    s_h2, s_h4 = int(s_h/2), int(s_h/4)
+    s_w2, s_w4 = int(s_w/2), int(s_w/4)
+    gfc_dim=1024
+
     with tf.variable_scope("generator", reuse=reuse):
         tl.layers.set_name_reuse(reuse)
+        
+        net_in = InputLayer(inputs, name='g/z_in')
+        y = InputLayer(y,name='g/y_in')
+        yb = ReshapeLayer(y, [batch_size, 1, 1, y_dim])
+        z = ConcatLayer([net_in, y], 1)
 
-        net_in = InputLayer(inputs, name='g/in')
-        net_h0 = DenseLayer(net_in, n_units=gf_dim*8*s16*s16, W_init=w_init,
+        net_h0 = DenseLayer(z, n_units=gfc_dim, W_init=w_initl, b_init=b_init,
                 act = tf.identity, name='g/h0/lin')
-        net_h0 = ReshapeLayer(net_h0, shape=[-1, s16, s16, gf_dim*8], name='g/h0/reshape')
         net_h0 = BatchNormLayer(net_h0, act=tf.nn.relu, is_train=is_train,
                 gamma_init=gamma_init, name='g/h0/batch_norm')
+        net_h0 = ConcatLayer([net_h0, y], 1, name='g/h0/concat')
 
-        net_h1 = DeConv2d(net_h0, gf_dim*4, (5, 5), out_size=(s8, s8), strides=(2, 2),
-                padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h1/decon2d')
+        net_h1 = DenseLayer(net_h0, n_units=gf_dim*2*s_h4*s_w4, W_init=w_initl, b_init=b_init,
+                act = tf.identity, name='g/h1/lin')
         net_h1 = BatchNormLayer(net_h1, act=tf.nn.relu, is_train=is_train,
                 gamma_init=gamma_init, name='g/h1/batch_norm')
+        net_h1 = ReshapeLayer(net_h1, shape=[batch_size, s_h4, s_w4, gf_dim * 2], name='g/h1/reshape')
+        net_h1 = condConcatLayer([net_h1, yb], 3, name='g/h1/concat') 
 
-        net_h2 = DeConv2d(net_h1, gf_dim*2, (5, 5), out_size=(s4, s4), strides=(2, 2),
+        net_h2 = DeConv2d(net_h1, gf_dim*2, (5, 5), out_size=(s_h2, s_w2), strides=(2, 2),
                 padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h2/decon2d')
         net_h2 = BatchNormLayer(net_h2, act=tf.nn.relu, is_train=is_train,
                 gamma_init=gamma_init, name='g/h2/batch_norm')
+        net_h2 = condConcatLayer([net_h2, yb], 3, name='g/h2/concat') 
 
-        net_h3 = DeConv2d(net_h2, gf_dim, (5, 5), out_size=(s2, s2), strides=(2, 2),
+        net_h3 = DeConv2d(net_h2, c_dim, (5, 5), out_size=(image_size, image_size), strides=(2, 2),
                 padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h3/decon2d')
-        net_h3 = BatchNormLayer(net_h3, act=tf.nn.relu, is_train=is_train,
-                gamma_init=gamma_init, name='g/h3/batch_norm')
+        
+        logits = net_h3.outputs
+        net_h3.outputs = tf.nn.sigmoid(net_h3.outputs)
+    return net_h3, logits
 
-        net_h4 = DeConv2d(net_h3, c_dim, (5, 5), out_size=(image_size, image_size), strides=(2, 2),
-                padding='SAME', batch_size=batch_size, act=None, W_init=w_init, name='g/h4/decon2d')
-        logits = net_h4.outputs
-        net_h4.outputs = tf.nn.tanh(net_h4.outputs)
-    return net_h4, logits
-
-def discriminator_simplified_api(inputs, is_train=True, reuse=False):
+def discriminator_simplified_api(inputs, y=None, is_train=True, reuse=False):
     df_dim = 64 # Dimension of discrim filters in first conv layer. [64]
-    c_dim = FLAGS.c_dim # n_color 3
+    y_dim = 10
+    c_dim = FLAGS.c_dim # n_color 1
     batch_size = FLAGS.batch_size # 64
     w_init = tf.random_normal_initializer(stddev=0.02)
+    w_initl = tf.random_normal_initializer(stddev=0.02)
+    b_init = tf.constant_initializer(0.0)
     gamma_init = tf.random_normal_initializer(1., 0.02)
     with tf.variable_scope("discriminator", reuse=reuse):
         tl.layers.set_name_reuse(reuse)
-
+        
         net_in = InputLayer(inputs, name='d/in')
-        net_h0 = Conv2d(net_in, df_dim, (5, 5), (2, 2), act=lambda x: tl.act.lrelu(x, 0.2),
-                padding='SAME', W_init=w_init, name='d/h0/conv2d')
+        y = InputLayer(y,name='g/y_in')
+        yb = ReshapeLayer(y, [batch_size, 1, 1, y_dim])
+        x = condConcatLayer([net_in, yb], 3, name='d/x/concat')
 
-        net_h1 = Conv2d(net_h0, df_dim*2, (5, 5), (2, 2), act=None,
+        net_h0 = Conv2d(x, c_dim + y_dim, (5, 5), (2, 2), act=lambda x: tl.act.lrelu(x, 0.2),
+                padding='SAME', W_init=w_init, name='d/h0/conv2d')
+        net_h0 = condConcatLayer([net_h0, yb], 3, name='d/h0/concat')
+
+        net_h1 = Conv2d(net_h0, df_dim + y_dim, (5, 5), (2, 2), act=None,
                 padding='SAME', W_init=w_init, name='d/h1/conv2d')
         net_h1 = BatchNormLayer(net_h1, act=lambda x: tl.act.lrelu(x, 0.2),
                 is_train=is_train, gamma_init=gamma_init, name='d/h1/batch_norm')
+        net_h1 = ReshapeLayer(net_h1, [batch_size, -1])
+        net_h1 = ConcatLayer([net_h1, y], 1, name='d/h1/concat')
 
-        net_h2 = Conv2d(net_h1, df_dim*4, (5, 5), (2, 2), act=None,
-                padding='SAME', W_init=w_init, name='d/h2/conv2d')
+        net_h2 = DenseLayer(net_h1, n_units=1024, act=tf.identity,
+                W_init = w_initl, b_init=b_init, name='d/h2/lin_sigmoid')
         net_h2 = BatchNormLayer(net_h2, act=lambda x: tl.act.lrelu(x, 0.2),
                 is_train=is_train, gamma_init=gamma_init, name='d/h2/batch_norm')
+        net_h2 = ConcatLayer([net_h2, y], 1, name='d/h2/concat')
 
-        net_h3 = Conv2d(net_h2, df_dim*8, (5, 5), (2, 2), act=None,
-                padding='SAME', W_init=w_init, name='d/h3/conv2d')
-        net_h3 = BatchNormLayer(net_h3, act=lambda x: tl.act.lrelu(x, 0.2),
-                is_train=is_train, gamma_init=gamma_init, name='d/h3/batch_norm')
+        net_h3 = DenseLayer(net_h2, n_units=1, act=tf.identity,
+                W_init = w_initl, b_init=b_init, name='d/h3/lin_sigmoid')
+        
+        logits = net_h3.outputs
+        net_h3.outputs = tf.nn.sigmoid(net_h3.outputs)
+    return net_h3, logits
 
-        net_h4 = FlattenLayer(net_h3, name='d/h4/flatten')
-        net_h4 = DenseLayer(net_h4, n_units=1, act=tf.identity,
-                W_init = w_init, name='d/h4/lin_sigmoid')
-        logits = net_h4.outputs
-        net_h4.outputs = tf.nn.sigmoid(net_h4.outputs)
-    return net_h4, logits
+class condConcatLayer(Layer):
+    def __init__(
+            self,
+            layers,
+            concat_dim=-1,
+            name='concat_layer',
+    ):
+        Layer.__init__(self, prev_layer=layers, name=name)
+        self.inputs = []
+        for l in layers:
+            self.inputs.append(l.outputs)
+
+        x = self.inputs[0]
+        y = self.inputs[1]
+        x_shapes = x.get_shape()
+        y_shapes = y.get_shape()
+
+        self.outputs = tf.concat([
+                x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], concat_dim)
+
+        self.all_layers.append(self.outputs)
+
+def load_mnist():
+    data_dir = os.path.join('./data', 'mnist')
+    
+    fd = open(os.path.join(data_dir,'train-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    trX = loaded[16:].reshape((60000,28,28,1)).astype(np.float)
+
+    fd = open(os.path.join(data_dir,'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    trY = loaded[8:].reshape((60000)).astype(np.float)
+
+    fd = open(os.path.join(data_dir,'t10k-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    teX = loaded[16:].reshape((10000,28,28,1)).astype(np.float)
+
+    fd = open(os.path.join(data_dir,'t10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    teY = loaded[8:].reshape((10000)).astype(np.float)
+
+    trY = np.asarray(trY)
+    teY = np.asarray(teY)
+    
+    X = np.concatenate((trX, teX), axis=0)
+    y = np.concatenate((trY, teY), axis=0).astype(np.int)
+    
+    seed = 547
+    np.random.seed(seed)
+    np.random.shuffle(X)
+    np.random.seed(seed)
+    np.random.shuffle(y)
+    
+    y_vec = np.zeros((len(y), 10), dtype=np.float)
+    for i, label in enumerate(y):
+      y_vec[i,y[i]] = 1.0
+    
+    return X/255.,y_vec
